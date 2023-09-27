b1cbdbe2374ea584556ee943c6e67174e42656ae
sususubin <133744604+sususubin@users.noreply.github.com>
2023-09-27 16:34:43 +0900

    Add files via upload

---
+++ 2302552.09.24.js
@@ -0,0 +1,21 @@
+console.log('my' + ' cat');
+console.log('1' + 2);
+console.log("ellis's \n\tbook");
+
+console.log(1 + 1); // add
+console.log(1 -1); // substract
+console.log(1 / 1); // divide
+console.log(1 * 1); // multiply
+console.log(5 % 2); // remaindr
+console.log(2 ** 3); // exponentiation 
+
+console.log(10 < 6); // less than
+console.log(10 <= 6); // less than or equal
+console.log(10 > 6); // greater than
+console.log(10 >= 6); // greater than or equal
+
+const value1 = true;
+const value2 = 4 < 2;
+console.log(value1 || valuw2 );
+console.log(value1 && value2 );
+
